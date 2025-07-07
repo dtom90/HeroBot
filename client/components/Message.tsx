cf3527eb5eae9437c8bd16623383c44090df9541
David Thomason <911671+dtom90@users.noreply.github.com>
2025-07-07 00:31:23 -0500

    adds message color

--- client/components/Message.tsx
+++ client/components/Message.tsx
@@ -2,12 +2,12 @@ import { View, Text } from 'react-native';
 import { Message as MessageType } from '../../shared/types';
 
 export const Message = ({ message }: { message: MessageType }) => {
-  const bgColor = message.type === 'user' ? 'bg-blue-200' : 'bg-green-200';
+  const bgColor = message.type === 'user' ? 'bg-blue-500' : 'bg-[#563D13]';
   const margin = message.type === 'user' ? 'ml-16' : 'mr-16';
   
   return (
     <View className={`flex-row mb-4 ${bgColor} ${margin} p-4 rounded-lg`}>
-      <Text>{message.text}</Text>
+      <Text className="text-white">{message.text}</Text>
     </View>
   );
 };
